61cbad50460dad20f0a160f1289f7643465bb5b8
Klimov-V <klimov_vitaliy@icloud.com>
2020-06-11 20:49:14 +0300

    added slider

--- js/script.js
+++ js/script.js
@@ -3,3 +3,29 @@ var headroom  = new Headroom(header, {
     tolerance: 10,
 });
 headroom.init();
+
+
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        prevArrow: `<button class="slider__arrow slider__arrow--prev btn btn--light btn--icon">
+                        <i class="fas fa-chevron-left"></i>
+                    </button>`,
+        nextArrow: `<button class="slider__arrow slider__arrow--next btn btn--light btn--icon">
+                        <i class="fas fa-chevron-right"></i>
+                    </button>`,
+        dotsClass: 'slider__dots',
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 960,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
